3869acdb77038ef59f6f9370e9ca65af31418cac
stexovat <107889434+stexovat@users.noreply.github.com>
2026-03-23 05:20:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const http = require('http');
+const https = require('https');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-  const targetUrl = url.parse(req.url);
+  const parsedUrl = url.parse(req.url);
+
+  const lib = parsedUrl.protocol === 'https:' ? https : http;
 
   const options = {
-    hostname: targetUrl.hostname,
-    port: 80,
-    path: targetUrl.path,
+    hostname: parsedUrl.hostname,
+    path: parsedUrl.path,
     method: req.method,
-    headers: req.headers,
+    headers: {
+      ...req.headers,
+      'user-agent': 'Mozilla/5.0',
+    },
   };
 
-  const proxy = http.request(options, (response) => {
+  const proxy = lib.request(options, (response) => {
     res.writeHead(response.statusCode, response.headers);
     response.pipe(res, { end: true });
+  });
+
+  proxy.on('error', () => {
+    res.writeHead(500);
+    res.end('Proxy error');
   });
 
   req.pipe(proxy, { end: true });
 });
 
 server.listen(3000, () => {
-  console.log('Proxy running on port 3000');
+  console.log('Proxy running');
 });
